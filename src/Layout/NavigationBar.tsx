543b2006a915adaa07d1a31ec50391bb4d0cf183
rodrigo_araujo42 <devcontractor_ra@bybox.com>
2023-02-06 10:16:11 -0300

    fixed nav bug

--- src/Layout/NavigationBar.tsx
+++ src/Layout/NavigationBar.tsx
@@ -1,36 +1,69 @@
-import { useContext } from 'react';
-import Container from 'react-bootstrap/Container';
-import Form from 'react-bootstrap/esm/Form';
-import Nav from 'react-bootstrap/Nav';
-import Navbar from 'react-bootstrap/Navbar';
-import { NavLink } from 'react-router-dom';
-import DarkModeIcon from '@mui/icons-material/DarkMode';
-import { ThemeContext, UpdateThemeContext } from '../Contexts/ThemeProvider';
-import "./styles/NavigationBar.css"
+import { useContext } from "react";
+import Container from "react-bootstrap/Container";
+import Form from "react-bootstrap/esm/Form";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+import { NavLink, useNavigate } from "react-router-dom";
+import DarkModeIcon from "@mui/icons-material/DarkMode";
+import { ThemeContext, UpdateThemeContext } from "../Contexts/ThemeProvider";
+import "./styles/NavigationBar.css";
 
 function NavigationBar(): JSX.Element {
   const { updateTheme } = useContext(UpdateThemeContext)!;
   const { isDarkMode } = useContext(ThemeContext);
-  
+  const navigate = useNavigate();
+
   return (
-    <Navbar fixed="top" bg={isDarkMode ? "dark" : "white"} variant={isDarkMode ? "dark" : "white"} expand="lg">
+    <Navbar
+      fixed="top"
+      bg={isDarkMode ? "dark" : "white"}
+      variant={isDarkMode ? "dark" : "white"}
+      expand="lg"
+    >
       <Container fluid>
-        <Navbar.Brand ><NavLink className={isDarkMode ? "NavigationLink-dark-mode" : "NavigationLink"} to={"/"}>B!og.</NavLink></Navbar.Brand>
+        <Navbar.Brand>
+          <NavLink
+            className={
+              isDarkMode ? "NavigationLink-dark-mode" : "NavigationLink"
+            }
+            to={"/"}
+          >
+            B!og.
+          </NavLink>
+        </Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
-            <Nav.Link className="navlink"><NavLink className={isDarkMode ? "NavigationLink-dark-mode" : "NavigationLink"} to={"/"}>Home</NavLink></Nav.Link>
-            <Nav.Link className="navlink"><NavLink className={isDarkMode ? "NavigationLink-dark-mode" : "NavigationLink"} to={"/register"}>Register</NavLink></Nav.Link>
+            <Nav.Link
+              onClick={() => navigate("/")}
+              className={`navlink ${
+                isDarkMode ? "NavigationLink-dark-mode" : "NavigationLink"
+              }`}
+            >
+              Home
+            </Nav.Link>
+            <Nav.Link
+              onClick={() => navigate("/register")}
+              className={`navlink ${
+                isDarkMode ? "NavigationLink-dark-mode" : "NavigationLink"
+              }`}
+            >
+              Register
+            </Nav.Link>
           </Nav>
           <Nav>
             <Form>
-              <Form.Check 
+              <Form.Check
                 type="switch"
                 id="custom-switch"
-                label={<DarkModeIcon style={{color: isDarkMode ? "white" : "black"}} />}
+                label={
+                  <DarkModeIcon
+                    style={{ color: isDarkMode ? "white" : "black" }}
+                  />
+                }
                 onChange={updateTheme}
               />
-              </Form>
+            </Form>
           </Nav>
         </Navbar.Collapse>
       </Container>
